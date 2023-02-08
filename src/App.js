f85f13384a24836309f4bf893de1138445dd64e4
Altemush Bhatti <altemush@gmail.com>
2023-02-08 15:48:31 -0800

    feat: create empty components and add CSS baseline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 const App = () => {
      return (
-        <div>
+        <>
+        
             <h1>Hi world!</h1>
-        </div>
+        </>
      )
 }
 
