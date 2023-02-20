fe975bcd2fcc7c2667303eedd0bbe77da3341039
Altemush Bhatti <altemush@gmail.com>
2023-02-19 23:29:04 -0800

    feat: set initial coordinates to user's current location

--- src/App.js
+++ src/App.js
@@ -9,8 +9,15 @@ import Map from './components/Map/Map';
 const App = () => {
     const [places, setPlaces] = useState([]);
 
-    const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+    const [coordinates, setCoordinates] = useState({});
     const [bounds, setBounds] = useState(null);
+
+    // set initial coordinates as user's current location
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+        })
+    }, []);
 
     useEffect(() => {
         console.log("effect", coordinates, bounds);
